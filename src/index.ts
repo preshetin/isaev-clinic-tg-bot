a90db6e041dbe6dfa61f17f0ceee3931a3415f76
Petr Reshetin <preshetin@gmail.com>
2024-10-25 14:55:33 +0300

    feat: some tweaks

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,17 @@ const bot = new Telegraf(BOT_TOKEN);
 bot.command('about', about());
 bot.on('message', greeting());
 
+// Handle button presses
+bot.action('yes', (ctx) => {
+  const originalMessage = ctx.update.callback_query.message as any;
+  console.log('11111', originalMessage)
+  ctx.editMessageText(`${originalMessage.text}\nYou pressed yes`);
+});
+
+bot.action('no', (ctx) => {
+  ctx.reply('You pressed No');
+});
+
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
   await production(req, res, bot);
--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -1,12 +1,17 @@
-import { Context } from 'telegraf';
+import { Context, Markup } from 'telegraf';
 import createDebug from 'debug';
 
 const debug = createDebug('bot:greeting_text');
 
 const replyToMessage = (ctx: Context, messageId: number, string: string) =>
-  ctx.reply(string, {
-    reply_parameters: { message_id: messageId },
-  });
+  // ctx.reply(string, {
+  //   reply_parameters: { message_id: messageId },
+  // });
+  ctx.reply(string, Markup.inlineKeyboard([
+    Markup.button.callback('Yes', 'yes'),
+    Markup.button.callback('No', 'no')
+  ]));
+
 
 const greeting = () => async (ctx: Context) => {
   debug('Triggered "greeting" text command');
@@ -15,7 +20,7 @@ const greeting = () => async (ctx: Context) => {
   const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
 
   if (messageId) {
-    await replyToMessage(ctx, messageId, `Hello, ${userName}!`);
+    await replyToMessage(ctx, messageId, `Hello 111, ${userName}!`);
   }
 };
 
